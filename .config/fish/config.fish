e876a5dfffa607e5cddbb6035f166d392859a7a2
lambehorn <ivanygnenok@gmail.com>
2025-07-30 14:23:30 +0500

    update fish grettings!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	function fish_greeting
+		echo "           __  _
+       .-.'  `; `-._  __  _
+      (_,         .-:'  `; `-._
+    ,'o\"( Hello!(_,            )
+   (__,-'      ,'o\"(            )>
+      (       (__,-'            )
+       `-'._.--._(             )
+          |||  |||`-'._.--._.-'
+                     |||  |||"
+	end
 end
+
